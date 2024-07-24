f8e79c8e65805bd9349f09c64bb74ec00d954b0c
Tejas Chendekar <tejas@Tejas-MacBook.local>
2024-07-24 05:41:18 +0100

    Added changes

--- train/batch_trainer/neural_ops_trainer.py
+++ train/batch_trainer/neural_ops_trainer.py
@@ -10,7 +10,7 @@ from model.fno import FNO
 import matplotlib.pyplot as plt
 
 class ModelTrainer:
-    def __init__(self, model, lr: float = 0.1, sigma: float = 0.001, k: int = 50, gamma: float = 0.1, max_iterations: Optional[int] = 1, loss_type: Optional[str] = 'mse', debug_mode = True):
+    def __init__(self, model, lr: float = 0.1, sigma: float = 0.01, k: int = 50, gamma: float = 0.1, max_iterations: Optional[int] = 1, loss_type: Optional[str] = 'mse', debug_mode = True):
         self.model = model
         self.loss_function_mapper = {
             'mse': nn.MSELoss(),
@@ -101,7 +101,7 @@ if __name__ == '__main__':
     width = 4
 
     model = FNO(in_channels, out_channels, modes, width)
-    model_trainer = ModelTrainer(model=model, lr=0.1, max_iterations=1, loss_type='mse', debug_mode=False)
+    model_trainer = ModelTrainer(model=model, lr=1, max_iterations=1, loss_type='mse', debug_mode=False)
     model_trainer.load_data(pde_data_loader)
     model_trainer.train(num_epochs=200, is_plot_graph=1)
     model_trainer.evaluate_test()
